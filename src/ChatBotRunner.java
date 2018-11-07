4588d185000ac5320c7d2a5624c1afd800ab9841
BT_1N3_16 <fwilking4736>
2018-11-07 08:18:23 -0500

    Arhz

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -16,11 +16,9 @@ public class ChatBotRunner
 		ChatBot1 chatbot1 = new ChatBot1();
 		ChatBot2 chatbot2 = new ChatBot2();
 		ChatBot3 chatbot3 = new ChatBot3();
-		
 
-		Scanner in = new Scanner (System.in);
 		System.out.println("Welcome to the chatbot, nice to meet you.");
 		chatbot1.chatLoop();
-		
+
 	}
 }
